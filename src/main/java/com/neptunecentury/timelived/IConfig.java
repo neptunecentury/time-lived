e6f4326431fde819fa5aabab4761337dd54ac6ff
neptunecentury <e.c.butler@outlook.com>
2024-10-29 22:31:02 -0400

    Update for 1.21.2

--- src/main/java/com/neptunecentury/timelived/IConfig.java
+++ src/main/java/com/neptunecentury/timelived/IConfig.java
@@ -1,6 +1,10 @@
 package com.neptunecentury.timelived;
 
 public interface IConfig {
+    /**
+     * Sets defaults and returns whether any updates were made that need saved.
+     * @return True if updates were made that need saved
+     */
     boolean setDefaults();
 
 }
